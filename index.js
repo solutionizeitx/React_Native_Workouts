a8aebe0b523b118254011498ada938bfb4ea3481
shabeebat <shabeeb.meizon@gmail.com>
2023-09-10 11:10:52 +0530

    accordin collapse module updated

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import Home from './src/home/Home';
+// import Home from './src/home/Home';
+import DropdownList from './src/dropdown/DropdownList';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => DropdownList);
